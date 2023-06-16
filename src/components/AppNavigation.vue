fb650904228825157ddae96800f9445a29eec051
UchVuch <85136486+UchVuch@users.noreply.github.com>
2023-06-17 00:26:49 +0300

    Add files via upload

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -6,16 +6,16 @@
     permanent
   >
     <v-list nav width="12vw">
-      <v-list-item prepend-icon="mdi-view-dashboard" title="Тендеры" value="tenders" @click="$router.push('/home')"></v-list-item>
+      <v-list-item prepend-icon="mdi-view-dashboard" title="Тендеры" @click="$router.push('/home')"></v-list-item>
     </v-list>
     <v-list nav width="12vw">
-      <v-list-item prepend-icon="mdi-view-dashboard" title="Генератор" value="generator" @click="$router.push('/generator')"></v-list-item>
+      <v-list-item prepend-icon="mdi-view-dashboard" title="Генератор" @click="$router.push('/generator')"></v-list-item>
     </v-list>
     <v-list nav width="12vw">
-      <v-list-item prepend-icon="mdi-view-dashboard" title="База данных" value="database" @click="$router.push('/database')"></v-list-item>
+      <v-list-item prepend-icon="mdi-view-dashboard" title="База данных" @click="$router.push('/database')"></v-list-item>
     </v-list>
     <v-list nav width="12vw">
-      <v-list-item prepend-icon="mdi-view-dashboard" title="Пользователи" value="users" @click="$router.push('/users')"></v-list-item>
+      <v-list-item prepend-icon="mdi-view-dashboard" title="Пользователи" @click="$router.push('/users')"></v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
